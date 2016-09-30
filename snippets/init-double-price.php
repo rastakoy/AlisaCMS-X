bf67efe80fe5cab989eaee51aae9b8fdea02a289
rastakoy <fruktstudio@gmail.com>
2016-09-30 04:39:25 +0300

    сниппеты в фильтрах (начало)

---
+++ snippets/init-double-price.php
@@ -0,0 +1,6 @@
+<table width="100%" cellpadding="0" cellspacing="0" border="0"><tr>
+	<td width="150" height="30"><?=$field["name$langPrefix"]?></td>
+	<td><input type="number" id="<?=$field['link']?>"
+	value="<?=$item[$field['link']]?>" onkeyup="globalEdit=true;" onchange="globalEdit=true;"
+	style="height:25px;width:100px;" /></td>
+</tr></table>
---
+++ snippets/init-varchar-3.php
@@ -0,0 +1,6 @@
+<table width="100%" cellpadding="0" cellspacing="0" border="0"><tr>
+	<td width="150" height="30"><?=$field["name$langPrefix"]?>
+	<b>(test)</b></td>
+	<td><input type="text" id="<?=$field['link']?>" style="width:100%;height:25px;"
+	value="<?=$item[$field['link']]?>" onkeyup="globalEdit=true;" onchange="globalEdit=true;" /></td>
+</tr></table>
