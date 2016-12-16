13e7060219e13d965c866f906b322a830398a140
alisiya <intert@rambler.ru>
2016-12-16 16:32:28 +0300

    partners_end

--- template/pages/partners.php
+++ template/pages/partners.php
@@ -6,9 +6,17 @@ if($mainItem['id']=='0'){
 <? //echo "<pro>"; print_r($mainItem['childrenItems']); echo "</pro>"; ?>
 
 	<? foreach($mainItem['childrenItems']['data'] as $item){ ?>
+		<?
+		if($item['tumb']['0']['name']){
+			$lnk = $classImages->createImageLink("loadimages", "215x195", $item['tumb']['0']['name']);
+		}
+		?>
 		<div style="border-top:#999999 1px dashed; margin-top:20px;">
+			<img src="<?=$lnk?>" border="0" style="margin-bottom:10px;width:215px;height:195px; float:left; padding-right:10px;" />
 			<a class="podrob" style="color:#333333;" href="<?=$item['link']?>"><h3><?=$item['name']?></h3></a>
+			<?=$item['content']?>
 		</div>
+		<div style="float:none; clear:both;"></div>
 	<? } ?>
 
 <? 
